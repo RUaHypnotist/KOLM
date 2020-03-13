7f654f3d0c745d9482bca54240a7c51a26d16eb4
RUaHypnotist <59844546+RUaHypnotist@users.noreply.github.com>
2020-03-13 18:20:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ var clickpower = 1;
 var meeplingPrice = 15;
 var mrmeepPrice = 100;
 var meepmagePrice = 1000;
+var megameepPrice = 50000;
+var demonmeepPrice = 100000;
+var meepgodPrice = 500000;
+var antimattermeepPrice = 1000000;
 var totalmrmps = 0;
 var totalmpmgs = 0;
+var totalmgmps = 0;
+var totaldmnmps = 0;
+var totalmpgds = 0;
+var totalntmtrs = 0;
 function initScore() {
  document.getElementById("header").innerHTML = "$" + score;
  setInterval(function(){
@@ -23,8 +31,32 @@ function initScore() {
   } else {
    document.getElementById("mpmageupbtn").style.filter = "brightness(50%)"
   };
-  score += totalmrmps/75;
-  score += totalmpmgs/5;
+  if (score >= megameepPrice) {
+   document.getElementById("mgmpupbtn").style.filter = "brightness(100%)";
+  } else {
+   document.getElementById("mgmpupbtn").style.filter = "brightness(50%)"
+  };
+  if (score >= demonmeepPrice) {
+   document.getElementById("dmnmpupbtn").style.filter = "brightness(100%)";
+  } else {
+   document.getElementById("dmnmpupbtn").style.filter = "brightness(50%)"
+  };
+  if (score >= meepgodPrice) {
+   document.getElementById("mpgdupbtn").style.filter = "brightness(100%)";
+  } else {
+   document.getElementById("mpgdupbtn").style.filter = "brightness(50%)"
+  };
+  if (score >= antimattermeepPrice) {
+   document.getElementById("ntmtrupbtn").style.filter = "brightness(100%)";
+  } else {
+   document.getElementById("ntmtrupbtn").style.filter = "brightness(50%)"
+  };
+  score += totalmrmps/100;
+  score += totalmpmgs/15;
+  score += totalmgmps*5;
+  score += totaldmnmps*150;
+  score += totalmpgds*10000;
+  score += totalntmtrs*1000000;
   document.getElementById("header").innerHTML = "$" + Math.round(score);
  }, 10);
 };
@@ -45,7 +77,8 @@ function mrmeepUp() {
   totalmrmps ++;
   score -= mrmeepPrice;
   mrmeepPrice *= 1.5;
-  document.getElementById("header").innerHTML = "$" + Math.round(score);
+ } else {
+  document.getElementById("mrmpupbtn").style.filter = "brightness(50%)"
  };
 };
 function meepmageUp() {
@@ -53,6 +86,43 @@ function meepmageUp() {
   totalmpmgs ++;
   score -= meepmagePrice;
   meepmagePrice *= 1.5;
-  document.getElementById("header").innerHTML = "$" + Math.round(score);
+ } else {
+  document.getElementById("mpmageupbtn").style.filter = "brightness(50%)"
+ };
+};
+function megameepUp() {
+ if (score >= meepmagePrice) {
+  totalmgmps ++;
+  score -= megameepPrice;
+  megameepPrice *= 1.5;
+ } else {
+  document.getElementById("mgmpupbtn").style.filter = "brightness(50%)"
+ };
+};
+function demonmeepUp() {
+ if (score >= demonmeepPrice) {
+  totaldmnmps ++;
+  score -= demonmeepPrice;
+  demonmeepPrice *= 1.5;
+ } else {
+  document.getElementById("dmnmpupbtn").style.filter = "brightness(50%)"
+ };
+};
+function meepgodUp() {
+ if (score >= meepgodPrice) {
+  totalmpgds ++;
+  score -= meepgodPrice;
+  meepgodPrice *= 1.5;
+ } else {
+  document.getElementById("mpgdupbtn").style.filter = "brightness(50%)"
+ };
+};
+function antimattermeepUp() {
+ if (score >= antimattermeepPrice) {
+  totalntmtrs ++;
+  score -= antimattermeepPrice;
+  antimattermeepPrice *= 1.5;
+ } else {
+  document.getElementById("ntmtrupbtn").style.filter = "brightness(50%)"
  };
 };
